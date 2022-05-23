2625049e0e28c49657ad7cf8ade1984e79064101
Bhaskarrd10 <bhaskarrd10@gmail.com>
2022-05-23 23:24:43 +0530

    added passwordgenartor

--- main.py
+++ main.py
@@ -6,26 +6,30 @@ from random import choice, randint, shuffle
 
 #Password Generator Project
 
-letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+def generate_password():
+
+
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
 
 
-password_letters = [choice(letters) for _ in range(randint(8, 10))]
-password_symbols = [choice(symbols) for _ in range(randint(2, 4))]
-password_numbers = [choice(numbers) for _ in range(randint(2, 4))]
+    password_letters = [choice(letters) for _ in range(randint(8, 10))]
+    password_symbols = [choice(symbols) for _ in range(randint(2, 4))]
+    password_numbers = [choice(numbers) for _ in range(randint(2, 4))]
 
-password_list = password_letters + password_symbols + password_numbers 
+    password_list = password_letters + password_symbols + password_numbers
 
-shuffle(password_list)
+    shuffle(password_list)
 
-password = "".join(password_list)
-#password = ""
-#for char in password_list:
-#  password += char
+    password = "".join(password_list)
+    password_entry.insert(0, password)
+    #password = ""
+    #for char in password_list:
+    #  password += char
 
-print(f"Your password is: {password}")
+    print(f"Your password is: {password}")
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def save():
@@ -81,7 +85,7 @@ password_entry = Entry(width=21)
 password_entry.grid(row=3, column=1)
 
 #Buttons
-generate_button = Button(text="Generate Password")
+generate_button = Button(text="Generate Password", command=generate_password)
 generate_button.grid(row=3, column=2)
 add_button = Button(text="ADD", width=36, command=save)
 add_button.grid(row=4, column=1, columnspan=2)
